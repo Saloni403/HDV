1bddc4b6d6f8169d92cec0381e75fcda524e7499
Saloni403 <156641454+Saloni403@users.noreply.github.com>
2025-08-05 17:19:53 +0530

    first

---
+++ web/js/script.js
@@ -0,0 +1,7 @@
+document.querySelector(".cross").addEventListener("click",function(){
+    document.querySelector(".menu").style.right="-350px"
+})
+
+document.querySelector(".listshow").addEventListener("click",function(){
+    document.querySelector(".menu").style.right="0"
+})
