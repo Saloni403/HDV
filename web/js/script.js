afa4fd5652862e96c66d4e51b38893672e532373
Saloni403 <156641454+Saloni403@users.noreply.github.com>
2025-08-07 18:20:52 +0530

    changes

--- web/js/script.js
+++ web/js/script.js
@@ -1,7 +1,18 @@
-document.querySelector(".cross").addEventListener("click",function(){
-    document.querySelector(".menu").style.right="-350px"
-})
+document.querySelector(".cross").addEventListener("click", function () {
+  document.querySelector(".menu").style.top = "-100%";
+});
 
-document.querySelector(".listshow").addEventListener("click",function(){
-    document.querySelector(".menu").style.right="0"
-})
+document.querySelector(".listshow").addEventListener("click", function () {
+  document.querySelector(".menu").style.top = "0";
+});
+let openmenusublist = true;
+document.querySelector(".menusublist").style.display = "none";
+function showMenuSublist() {
+  if (openmenusublist) {
+    document.querySelector(".menusublist").style.display = "block";
+    openmenusublist = false;
+  } else {
+    document.querySelector(".menusublist").style.display = "none";
+    openmenusublist = true;
+  }
+}
