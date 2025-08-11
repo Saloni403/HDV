48312a10c857d5c79f48ddca1bacd31adaa19867
Saloni403 <156641454+Saloni403@users.noreply.github.com>
2025-08-11 15:04:15 +0530

    changes

--- web/js/script.js
+++ web/js/script.js
@@ -16,3 +16,58 @@ function showMenuSublist() {
     openmenusublist = true;
   }
 }
+
+
+
+
+
+
+
+
+function declineTerms() {
+    // If there is a previous page in history
+    if (document.referrer !== "") {
+      history.back();
+    } else {
+      // Fallback: redirect to homepage or any specific page
+      window.location.href = "index.html"; // change this to your desired page
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+function redirectToAlbum(event){
+let title = event.querySelector("h6").innerText;
+let imgsrc = event.querySelector("img").src;
+// console.log(title);
+ let encodedTitle = encodeURIComponent(title);
+ let encodedImg = encodeURIComponent(imgsrc);
+window.location.href="album.html?title="+encodedTitle+"&img="+encodedImg;
+}
+
+
+function getQueryParam(param) {
+  let urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(param);
+}
+let albumTitle = getQueryParam("title");
+let albumImg = getQueryParam("img");
+if (albumTitle) {
+  // document.getElementsByClassName("albumTitle").innerText = albumTitle;
+let classabmtitle= document.getElementsByClassName("albumTtl")
+for(let i=0; i<classabmtitle.length;i++){
+classabmtitle[i].innerText= albumTitle;
+}
+}
+if(albumImg){
+let classabmImg=document.getElementById("aboutbanner2")
+ classabmImg.style.backgroundImage = `url('${albumImg}')`;
+}
